f8b152e4d271f63b1807fc2a016a7420bce97f04
muellerdom <do161mue@htwg-konstanz.de>
2026-05-23 16:46:51 +0200

    fix test hall

--- test/test_hall.cpp
+++ test/test_hall.cpp
@@ -5,30 +5,7 @@
 #include "hall_angle.h"
 
 // ─────────────────────────────────────────────────────────────
-float wrapAngle(float a) {
-    while (a <    0.0f) a += 360.0f;
-    while (a >= 360.0f) a -= 360.0f;
-    return a;
-}
-
-// ─────────────────────────────────────────────────────────────
-/*uint8_t readAGC() {
-    Wire.beginTransmission(AS5600_ADDR);
-    Wire.write(AGC_REG);
-    Wire.endTransmission(false);
-    Wire.requestFrom(AS5600_ADDR, 1);
-    return Wire.available() ? Wire.read() : 0;
-}
-
-// ─────────────────────────────────────────────────────────────
-uint16_t readMagnitude() {
-    Wire.beginTransmission(AS5600_ADDR);
-    Wire.write(MAG_REG);
-    Wire.endTransmission(false);
-    Wire.requestFrom(AS5600_ADDR, 2);
-    uint16_t magnitude = (Wire.read() << 8) | Wire.read();
-    return magnitude & 0x0FFF;
-} */
+static HallAngle sensor;
 
 // ─────────────────────────────────────────────────────────────
 void setup() {
@@ -51,15 +28,8 @@ void setup() {
 void loop() {
     if (!sensor.update()) return;
 
-        float raw = readRawAngle();
-        if (raw < 0.0f) return;
-
-        raw = wrapAngle(raw - offset);
-
-        // ── Teleplot output ───────────────────────────────────
-        Serial.printf(">as5600/angle_raw: %.2f\n",       raw);
-        //Serial.printf(">as5600/magnitude: %d\n",         readMagnitude());
-        //Serial.printf(">as5600/agc: %d\n",               readAGC());
-    }
-    delay(5);
+    Serial.printf(">as5600/angle_raw: %.2f\n",       sensor.getAngleRaw());
+    Serial.printf(">as5600/angle_with_calc: %.2f\n", sensor.getAngleCalc());
+    Serial.printf(">as5600/magnitude: %d\n",         sensor.getMagnitude());
+    Serial.printf(">as5600/agc: %d\n",               sensor.getAGC());
 }
