65477250b1290c0771f01837f9febfba6674689e
Jaken <you@example.com>
2026-06-16 15:05:15 +0200

    kleiner nachtrag mit dummy daten

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -7,11 +7,11 @@ void Sensor::begin() {
 }
 
 float Sensor::ReadForce() {
-  const int force = forceReader.sampleForce();
-  return (1.03234f * 3.3f) / 4095.0f;
+  //const int force = forceReader.sampleForce();
+  return (1.03234f * 2.3f) / 4095.0f;
 }
 
 float Sensor::ReadAngle() {
-  const float angle = angleReader.sampleAndCalculateAngle();
+  //const float angle = angleReader.sampleAndCalculateAngle();
   return (1.03234f * 3.3f) / 4095.0f;
 }
